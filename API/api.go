c7c5a89b267c86fdf85f70e053d25c6bd45ccb56
christinavanmoof <97024402+christinavanmoof@users.noreply.github.com>
2022-08-23 10:19:47 +0200

    Add headers

--- API/api.go
+++ API/api.go
@@ -19,6 +19,8 @@ func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.once.Do(func() {
 		mux := http.NewServeMux()
 		mux.HandleFunc("/generate", func(rw http.ResponseWriter, _ *http.Request) {
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
 			rw.Write([]byte(a.Gen.Generate())) // Write the generated password to the response writer
 		})
 		mux.ServeHTTP(w, r)
